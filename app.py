e48edc7055901bf03869561133ed8de68da910df
Shivam <Shivamkumar188132@gmail.com>
2024-06-19 17:55:46 +0530

    'final'

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import streamlit as st
 import pickle
-from sklearn.preprocessing import StandardScaler
 
 # Load the model and scaler from disk
 with open('best_model.pkl', 'rb') as model_file:
@@ -19,18 +18,25 @@ def predict_cancer(age, gender, bmi, smoking, genetic_risk, physical_activity, a
     return prediction[0]
 
 # Streamlit app
-st.title("Cancer Prediction App")
+st.title(":red[Cancer] Prediction :blue[App]")
 
-st.write("Please enter the following details:")
-
-age = st.number_input("Age", min_value=20, max_value=80, step=1)
-gender = st.radio("Gender", options=[0, 1], format_func=lambda x: 'Male' if x == 0 else 'Female')
-bmi = st.slider("BMI", min_value=15.0, max_value=40.0, step=0.1)
-smoking = st.radio("Smoking", options=[0, 1], format_func=lambda x: 'No' if x == 0 else 'Yes')
-genetic_risk = st.selectbox("Genetic Risk", options=[0, 1, 2], format_func=lambda x: ['Low', 'Medium', 'High'][x])
-physical_activity = st.slider("Physical Activity (hours/week)", min_value=0.0, max_value=10.0, step=0.1)
-alcohol_intake = st.slider("Alcohol Intake (units/week)", min_value=0.0, max_value=5.0, step=0.1)
-cancer_history = st.radio("Cancer History", options=[0, 1], format_func=lambda x: 'No' if x == 0 else 'Yes')
+st.write(":green[Please enter the following details:]")
+st.markdown("**👴 Age**")
+age = st.number_input("", min_value=20, max_value=80, step=1)
+st.markdown("**👦🏻/👩🏻 :green[Gender]**")
+gender = st.radio("", options=[0, 1], format_func=lambda x: 'Male' if x == 0 else 'Female')
+st.markdown("**📇 :green[Body Mass Index](BMI)**")
+bmi = st.slider("", min_value=15.0, max_value=40.0, step=0.05)
+st.markdown("**😗🚬 :red[Smoking]**")
+smoking = st.radio("", options=[0, 1], format_func=lambda x: 'No' if x == 0 else 'Yes')
+st.markdown("**🧬 :blue[Genetic Risk]**")
+genetic_risk = st.selectbox("", options=[0, 1, 2], format_func=lambda x: ['Low', 'Medium', 'High'][x])
+st.markdown("**🏋🏽🔥💪🏼🎧 :green[Physical Activity ](hours/week)**")
+physical_activity = st.slider("", min_value=0.0, max_value=10.0, step=0.05)
+st.markdown("**🍻 :red[Alcohol Intake ](units/week)**")
+alcohol_intake = st.slider("", min_value=0.0, max_value=5.0, step=0.05)
+st.markdown("**🦀 :blue[Cancer History]**")
+cancer_history = st.radio(" ", options=[0, 1], format_func=lambda x: 'No' if x == 0 else 'Yes')
 
 if st.button("Predict"):
     result = predict_cancer(age, gender, bmi, smoking, genetic_risk, physical_activity, alcohol_intake, cancer_history)
