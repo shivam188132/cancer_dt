b599b7390db51f489eee04874f7b8a3ddd126e3c
Shivam <Shivamkumar188132@gmail.com>
2024-06-20 12:15:16 +0530

    '20jun'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pickle
 with open('best_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-with open('scaler (1).pkl', 'rb') as scaler_file:
+with open('scaler.pkl', 'rb') as scaler_file:
     scaler = pickle.load(scaler_file)
 
 # Function to make predictions
